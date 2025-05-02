fab50661560ba8077cbe82c3869de06ceec8505a
Alexey Zhmur <teomen@yandex.ru>
2025-05-02 20:04:47 +0300

    flake8 .

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from api import views
 from rest_framework import routers
 
-from api import views
 
 router = routers.DefaultRouter()
 router.register('tasks', views.TaskView, 'task')
